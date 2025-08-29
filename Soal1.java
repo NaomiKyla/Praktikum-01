d12b9d7ac536abfe24934ae4650bbfb4cd3192bb
naomi kyla <naomikyla@nomay-2.local>
2025-08-29 16:56:10 +0700

    Soal 1 - Praktikum 01

---
+++ Soal1.java
@@ -0,0 +1,10 @@
+public class Soal1 {
+    public static void main(String[] args) {
+        System.out.println("         ._,    ");
+        System.out.println("       _('v')_ ");
+        System.out.println("     `\"-\\._./-\"' ");
+        System.out.println("         ^ ^     ");
+        System.out.println("----------------------------");
+        System.out.println("Naomi Kyla Zahra Siregar \uD83E\uDD89 (NPM: 2406434102)");
+    }
+}
